8dfbf02108ea0e86a28cece187a175777b032e5c
Naomi-Pate <patenaomi99@gmail.com>
2023-08-04 12:24:57 -0400

    Updated db

---
+++ Steam-Store-back-end/db/seed.sql
@@ -0,0 +1,11 @@
+
+\c games_dev;
+
+-- COPY NEWTABLE FROM :'localpath' csv HEADER;
+
+COPY games (appid,name,release_date,english,developer,publisher,platforms,required_age,categories,genres,steamspy_tags,achievements,positive_ratings,negative_ratings,average_playtime,median_playtime,owners,price)
+FROM '/Users/naomi/desktop/module4/Projects/Project-Single-Resource-Steam-Store/Steam-Store-back-end/db/steam.csv' 
+DELIMITER ',' 
+CSV HEADER;
+
+SELECT * FROM games;
