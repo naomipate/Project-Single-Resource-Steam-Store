57230e4086f88a371f073ef716cf296fc8009dd2
AkiraBrown <akira.watson.brown@gmail.com>
2023-08-07 11:04:16 -0400

    Fixed backend route-AB

--- Steam-Store-front-end/steam-frontend/src/components/Home/Home.js
+++ Steam-Store-front-end/steam-frontend/src/components/Home/Home.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getAllGames } from "../common/API/API";
+import Overlay from "../common/overlay/Overlay";
 
 function Home() {
-  return <div>Home</div>;
+  const [gameArray, setGameArray] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  async function fetchData() {
+    try {
+      setIsLoading(true);
+      let result = await getAllGames();
+      setGameArray(result.data);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  return (
+    <Overlay isLoading={isLoading}>
+      <div className="container">
+        {gameArray.map(({ name, id }) => {
+          return <div key={id}>{name}</div>;
+        })}
+      </div>
+      ;
+    </Overlay>
+  );
 }
 
 export default Home;
