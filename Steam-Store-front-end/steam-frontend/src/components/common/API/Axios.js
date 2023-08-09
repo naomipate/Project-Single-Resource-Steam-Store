49580052c8e6a3a221003796d3eb7b5d876cec18
Naomi-Pate <patenaomi99@gmail.com>
2023-08-09 19:28:50 -0400

    updated Deploy URL

--- Steam-Store-front-end/steam-frontend/src/components/common/API/Axios.js
+++ Steam-Store-front-end/steam-frontend/src/components/common/API/Axios.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const AxiosInstance = axios.create({
   baseURL:
     process.env.NODE_ENV === "development"
-      ? "http://localhost:3003"
-      : "DEPLOY URL",
+      ? process.env.NODE_ENV.REACT_APP_API_URL_LOCAL
+      : "https://project-single-resource-steam-store.onrender.com",
   timeout: 50000,
 });
 
